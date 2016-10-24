3ef5c76cbaf577a67a77b212ae7c6d9d46f08a13
eschreibman <eliza.schreibman@gmail.com>
2016-10-24 15:16:56 -0400

    Updated Unity and added a test script

---
+++ BabyGuidoProject/Assets/Scripts/testScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+	//THIS SCRIPT DOES NOTHING
+	//JUST FOR TESTING GIT & UNITY
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
