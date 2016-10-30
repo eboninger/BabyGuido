4c7b1d2335ba9c46a4f1015bcef1cc6c711b250e
Eli Boninger <eboninger@gmail.com>
2016-10-30 19:45:41 -0400

    Sounds play in order

---
+++ BabyGuidoProject/Assets/Scripts/playSequence.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playSequence : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        var hand = GameObject.Find("Hand").transform;
+
+        StartCoroutine(PlaySoundList(hand));
+        //hand.GetChild(i).GetComponent<AudioSource>().Play();
+
+    }
+
+    IEnumerator PlaySoundList(Transform hand)
+    {
+        int numSounds = hand.childCount;
+        for (int i = 0; i < numSounds; i++)
+        {
+            AudioSource audio = hand.GetChild(i).GetComponent<AudioSource>();
+            audio.Play();
+            yield return new WaitForSecondsRealtime(audio.clip.length);
+        }
+        
+    }
+}
