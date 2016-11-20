20f25310731127704b01da007bc13a47dd832fee
eschreibman <eliza.schreibman@gmail.com>
2016-11-20 17:51:01 -0500

    Able to drag and drop bird on staff (have not tested mobile).

--- LookSound/Assets/Scripts/generateChild.cs
+++ LookSound/Assets/Scripts/generateChild.cs
@@ -3,20 +3,16 @@ using System.Collections;
 
 public class generateChild : MonoBehaviour {
 	public GameObject childTemplate;
-
-	private GameObject myChild;
 	private Transform childTransform;
 
-	// Use this for initialization
-	void Start () {
-		childTransform = transform.GetChild(0);
-		myChild = childTransform.gameObject;
-	}
-
-	public void makeNewChild(){
+	public void makeNewChild(int index){
 		//create a new child
 		GameObject newChild = Instantiate(childTemplate);
 		newChild.transform.SetParent(transform);
+		if(index == 0){
+			newChild.transform.SetAsFirstSibling();
+		} 
 		newChild.transform.position = transform.position;
 	}
+
 }
