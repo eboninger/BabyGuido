79cdcc974a24f8725dc3481b061319032755e615
eschreibman <eliza.schreibman@gmail.com>
2016-11-19 21:53:01 -0500

    Got mouse movement with touch working on pushing to Android.

---
+++ LookSound/Assets/Scripts/generateChild.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generateChild : MonoBehaviour {
+	public GameObject childTemplate;
+
+	private GameObject myChild;
+	private Transform childTransform;
+
+	// Use this for initialization
+	void Start () {
+		childTransform = transform.GetChild(0);
+		myChild = childTransform.gameObject;
+	}
+
+	public void makeNewChild(){
+		//create a new child
+		GameObject newChild = Instantiate(childTemplate);
+		newChild.transform.SetParent(transform);
+	}
+}
---
+++ LookSound/Assets/Scripts/touchHand.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Moves the hand to where the player clicks/touches
+public class touchHand : MonoBehaviour {
+	//Public Variables
+	public GameObject hand;
+	public GameObject[] draggables;
+
+	//Private Variables
+	private Vector3 handPos;
+	private bool touched = false;
+
+
+	void Start (){
+		//get initial position
+		handPos = hand.transform.position;
+		int i = 0;
+		foreach(GameObject o in draggables){
+			if(o != null){
+				i++;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update (){
+		//If running game in editor
+		#if UNITY_EDITOR
+		//Using mouse
+		mouseDrag();
+
+		#endif
+		//Using phone/tablet touch
+		if (Input.touchCount >= 1){
+			touchDrag();
+		}
+	}
+
+	//FixedUpdate is called once per fixed time step
+	void FixedUpdate(){
+		if (touched){
+			//now move the objects to their new position
+			hand.transform.position = handPos;
+			touched = false;
+		}
+	}
+
+
+	//move the mouse to where the player touches
+	void touchDrag() {
+		Touch touch = Input.touches[0];
+		Vector3 pos = touch.position;
+		pos.z = 10;
+
+		Vector3 screenPos = Camera.main.ScreenToWorldPoint(pos);
+
+		RaycastHit2D hit = Physics2D.Raycast(screenPos,Vector2.zero);
+
+		if(hit){
+			handPos = screenPos;
+			touched = true;	
+		}
+	}
+
+	//for debugging, use the mouse as input
+	void mouseDrag(){
+		
+		if(Input.GetMouseButton(0)) {
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = 10;
+
+			Vector3 screenPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+			RaycastHit2D hit = Physics2D.Raycast(screenPos,Vector2.zero);
+
+			if(hit){
+				print (hit.collider.name);
+			}
+			handPos = screenPos;
+		}
+		touched = true;	
+	}
+
+
+}
