167976d32bad0990469e34afc5cc95d9f69d4a02
eschreibman <eliza.schreibman@gmail.com>
2016-12-04 18:54:49 -0500

    Added new wave art and put bir character in touch scene

---
+++ LookSound/Assets/Scripts/playStaffSounds.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playStaffSounds : MonoBehaviour {
+	public GameObject staff;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void play(){
+		print("in play");
+		foreach(Transform child in staff.transform){
+			print("play " + child.name + "s sound");
+		}
+
+
+	}
+}
