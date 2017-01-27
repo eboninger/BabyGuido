ee1140691983d47e85a4cfd5a67e70af5bdcb285
eschreibman <eliza.schreibman@gmail.com>
2017-01-27 18:31:57 -0500

    Added apple and banana and staff

---
+++ LookSound/Assets/Scripts/Fruit Scripts/addFruitToStaff.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addFruitToStaff : MonoBehaviour {
+	public GameObject apple;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		keyPressed();
+	}
+
+	void keyPressed(){
+		if (Input.GetKeyDown(KeyCode.A)){
+			
+		}
+	}
+
+	//check to see what key is pressed
+
+	//add a fruit to the staff
+
+	//see where the fruit needs to be placed
+}
