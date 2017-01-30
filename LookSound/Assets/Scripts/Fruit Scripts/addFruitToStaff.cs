a0f8435d2a24eb45a3384e6c2364d47332f0d7cb
eschreibman <eliza.schreibman@gmail.com>
2017-01-29 23:42:04 -0500

    Can add apple to staff. Apple fades away over a few seconds and then dissapears

--- LookSound/Assets/Scripts/Fruit Scripts/addFruitToStaff.cs
+++ LookSound/Assets/Scripts/Fruit Scripts/addFruitToStaff.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class addFruitToStaff : MonoBehaviour {
+	
 	public GameObject apple;
+	private Vector3[] locationsOnStaff = new [] { new Vector3(10f,13.33f,0f), 
+												  new Vector3(1f,1f,1f) };
 	// Use this for initialization
 	void Start () {
 	
@@ -14,9 +17,29 @@ public class addFruitToStaff : MonoBehaviour {
 	}
 
 	void keyPressed(){
-		if (Input.GetKeyDown(KeyCode.A)){
-			
+		if (Input.anyKeyDown)
+		{
+			foreach (char c in Input.inputString)
+			{
+				if(c == 'a'){
+					//make a new apple
+					GameObject newA = Instantiate(apple);
+					//move it to the correct position on the staff
+					newA.transform.position = locationsOnStaff[0];
+					//make the apple fade over time
+					fadeFruit ff = newA.GetComponent<fadeFruit>();
+					if(ff != null){
+						ff.turnOnFade();
+					} else{
+						print("Error: no fade fruit script attached to " + newA.name);
+					}
+
+				}
+			}
 		}
+//		if (Input.GetKeyDown(KeyCode.A)){
+//			
+//		}
 	}
 
 	//check to see what key is pressed
---
+++ LookSound/Assets/Scripts/Fruit Scripts/fadeFruit.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadeFruit : MonoBehaviour {
+	float minimum = 0.0f;
+	float maximum = 1f;
+	float duration = 3.0f;
+	bool fade = false;
+	float startTime;
+	float t;
+	float transparency;
+	SpriteRenderer sprite;
+
+	public void turnOnFade(){
+		fade = true;
+		startTime = Time.time;
+		sprite = GetComponentInChildren<SpriteRenderer>();
+	}
+
+	public void turnOffFade(){
+		fade = false;
+	}
+	// Update is called once per frame
+	void Update () {
+		if(fade){
+			t = (Time.time - startTime) / duration;
+			//fade the transparency over time
+			transparency = Mathf.SmoothStep(maximum, minimum, t);
+			sprite.color = new Color(1f, 1f, 1f, transparency);
+			//when the object is no longer visible, destory it
+			if(transparency <= 0){
+				Destroy(gameObject);
+			}
+
+		}
+	
+	}
+}
