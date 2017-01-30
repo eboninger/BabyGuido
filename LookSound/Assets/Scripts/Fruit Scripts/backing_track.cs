16eceb1f23732f2baf60b1a767674a0a73189cce
Eli Boninger <eboninger@gmail.com>
2017-01-29 19:13:59 -0500

    on the beat stuff started

---
+++ LookSound/Assets/Scripts/Fruit Scripts/backing_track.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backing_track : MonoBehaviour {
+
+    public AudioSource bt;
+    public const float BEAT_MOD = 0.5f;
+    public const float BEAT = 0.314f;
+    public const float DIF = 0.075f;
+    public const float BEAT_LO = BEAT - DIF;
+    public const float BEAT_HI = BEAT + DIF;
+
+    // Use this for initialization
+    void Start()
+    {
+        bt.Play();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool checkOnBeat()
+    {
+        var ts_mod = bt.time % BEAT_MOD;
+        if ((ts_mod > BEAT_LO) && (ts_mod < BEAT_HI))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ LookSound/Assets/Scripts/Fruit Scripts/score.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class score : MonoBehaviour {
+
+    public backing_track bt;
+    public Text score_notification;
+    public Dictionary<string,AudioSource> notes;
+    int on_streak = 0;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        notes = new Dictionary<string, AudioSource>();
+        var sources = this.GetComponentsInParent<AudioSource>();
+
+        notes.Add("a", sources[1]);
+        notes.Add("s", sources[2]);
+        notes.Add("d", sources[3]);
+        notes.Add("f", sources[4]);
+        notes.Add("g", sources[5]);
+        notes.Add("h", sources[6]);
+        notes.Add("j", sources[7]);
+        print(notes);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (char c in Input.inputString)
+            {
+                //notes[c.ToString()].Play();
+                beat_check();
+            }
+        }
+        
+    }
+
+    void beat_check()
+    {
+        if (bt.checkOnBeat())
+        {
+            on_streak++;
+            if (on_streak > 3)
+            {
+                score_notification.text = "On the beat streak! Length " + on_streak.ToString();
+                score_notification.enabled = true;
+            }
+        }
+        else
+        {
+            on_streak = 0;
+            score_notification.enabled = false;
+        }
+    }
+
+}
