e4a55e075e9240650dea34f9f7801776b207c87d
eschreibman <eliza.schreibman@gmail.com>
2017-01-31 13:05:56 -0500

    Added more fruit. Made adding fruit to staff code more modular.

--- LookSound/Assets/Scripts/Fruit Scripts/addFruitToStaff.cs
+++ LookSound/Assets/Scripts/Fruit Scripts/addFruitToStaff.cs
@@ -1,13 +1,43 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+
+public class fruitWithLocation{
+	private GameObject o;
+	private Vector3 pos;
+
+	public fruitWithLocation(GameObject gameObj, Vector3 location){
+		o = gameObj;
+		pos = location;
+	}
+
+	public GameObject getObject(){
+		return o;
+	}
+
+	public Vector3 getPosition(){
+		return pos;
+	}
+}
 
 public class addFruitToStaff : MonoBehaviour {
 	
 	public GameObject apple;
-	private Vector3[] locationsOnStaff = new [] { new Vector3(10f,13.33f,0f), 
-												  new Vector3(1f,1f,1f) };
+	public GameObject banana;
+	public GameObject cherry;
+	public GameObject date;
+	public GameObject eggplant;
+	private Dictionary<char, fruitWithLocation> fruit;
 	// Use this for initialization
 	void Start () {
+		fruit = new Dictionary<char, fruitWithLocation>();
+		fruitWithLocation a = new fruitWithLocation(apple, new Vector3 (10f,13.33f,0f));
+		fruitWithLocation b = new fruitWithLocation(banana, new Vector3 (8.6f,9.5f,0f));
+		fruitWithLocation c = new fruitWithLocation(cherry, new Vector3 (11.6f,10f,0f));
+		fruit.Add('a', a);
+		fruit.Add('b', b);
+		fruit.Add('c', c);
 	
 	}
 	
@@ -21,30 +51,34 @@ public class addFruitToStaff : MonoBehaviour {
 		{
 			foreach (char c in Input.inputString)
 			{
-				if(c == 'a'){
-					//make a new apple
-					GameObject newA = Instantiate(apple);
-					//move it to the correct position on the staff
-					newA.transform.position = locationsOnStaff[0];
-					//make the apple fade over time
-					fadeFruit ff = newA.GetComponent<fadeFruit>();
-					if(ff != null){
-						ff.turnOnFade();
-					} else{
-						print("Error: no fade fruit script attached to " + newA.name);
-					}
+				createObject(c);
 
-				}
 			}
 		}
-//		if (Input.GetKeyDown(KeyCode.A)){
-//			
-//		}
 	}
 
-	//check to see what key is pressed
+	//instantiate a new fruit prefab based on the keyboard key the user pressed
+	void createObject(char c){
+		GameObject newObj;
+		try {
+			newObj = Instantiate(fruit[c].getObject());
+			//move it to the correct position on the staff
+			newObj.transform.position = fruit[c].getPosition();
+			fadeOn(newObj);
+		}
+		catch (Exception e) {
+			print("Error: no fruit associated with " + c.ToString());
+		}  
+	}
 
-	//add a fruit to the staff
+	//make the fruit fade over time
+	void fadeOn(GameObject newObj){
+		fadeFruit ff = newObj.GetComponent<fadeFruit>();
+		if(ff != null){
+			ff.turnOnFade();
+		} else{
+			print("Error: no fade fruit script attached to " + newObj.name);
+		}
+	}
 
-	//see where the fruit needs to be placed
 }
