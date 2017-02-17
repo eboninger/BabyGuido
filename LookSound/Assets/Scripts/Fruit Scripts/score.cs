3406897a6c51d3f3bb0ec983e22eebc3ea638f5d
Eli Boninger <eboninger@gmail.com>
2017-02-17 13:25:39 -0500

    more scoring mechanisms and messages

--- LookSound/Assets/Scripts/Fruit Scripts/score.cs
+++ LookSound/Assets/Scripts/Fruit Scripts/score.cs
@@ -3,35 +3,44 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+public class Note
+{
+    public AudioSource sample;
+    public int num;
+
+    public Note(AudioSource a, int n)
+    {
+        sample = a;
+        num = n;
+    }
+}
+
 public class score : MonoBehaviour {
 
     public backing_track bt;
-    public Text score_notification;
-    public Text chord_notification;
-    public Dictionary<string,AudioSource> notes;
-    int on_streak = 0;
-    int off_streak = 0;
-    bool on_chord = false;
-    bool first_press = true;
+    public Text rhythm_notification, chord_notification, scale_notification;
+    public Dictionary<string, Note> notes;
+    int on_streak = 0, off_streak = 0, prev_note = -10, scale_up = 0, scale_down = 0, user_score = 0;
+    bool on_chord = false, first_press = true, on_scale = false;
+    public Note input_note;
 
 
 	// Use this for initialization
 	void Start ()
     {
-        notes = new Dictionary<string, AudioSource>();
+        notes = new Dictionary<string, Note>();
         var sources = this.GetComponentsInParent<AudioSource>();
         chord_notification.enabled = false;
         chord_notification.text = "Great Chord!";
 
-        notes.Add("a", sources[1]);
-        notes.Add("s", sources[2]);
-        notes.Add("d", sources[3]);
-        notes.Add("f", sources[4]);
-        notes.Add("g", sources[5]);
-        notes.Add("h", sources[6]);
-        notes.Add("j", sources[7]);
-        print(notes);
-
+        notes.Add("a", new Note(sources[1], 0));
+        notes.Add("s", new Note(sources[2], 1));
+        notes.Add("d", new Note(sources[3], 2));
+        notes.Add("f", new Note(sources[4], 3));
+        notes.Add("g", new Note(sources[5], 4));
+        notes.Add("h", new Note(sources[6], 5));
+        notes.Add("j", new Note(sources[7], 6));
+        notes.Add("k", new Note(sources[8], 7));
     }
 	
 	// Update is called once per frame
@@ -39,32 +48,78 @@ public class score : MonoBehaviour {
     {
         if (Input.anyKeyDown)
         {
+            // to calibrate the rhythm
             if (first_press)
             {
                 bt.setBeat();
                 first_press = false;
             }
+
+            // play each note corresponding to buttons pressed
             foreach (char c in Input.inputString)
             {
-                //notes[c.ToString()].PlayOneShot(notes[c.ToString()].clip, 1);
+                input_note = notes[c.ToString()];
+                input_note.sample.Play();
             }
-            beat_check();
-
-            if ((Input.inputString.Length > 1) && (!on_chord))
-            {
-                StartCoroutine(chord_played());
-            }
+            give_points();
         }
-        
+
     }
 
-    IEnumerator chord_played()
+    private void give_points()
     {
-        chord_notification.enabled = true;
-        on_chord = true;
-        yield return new WaitForSeconds(2);
-        on_chord = false;
-        chord_notification.enabled = false;
+        beat_check();
+
+        // if a chord is played
+        if ((Input.inputString.Length > 1))
+        {
+            StartCoroutine(notification(chord_notification, "Great Chord!! +5", on_chord, 5));
+            scale_up = 0;
+            scale_down = 0;
+        }
+        // if the note played is one above the previous note
+        else if (input_note.num == (prev_note + 1))
+        {
+            scale_up++;
+        }
+        // if the note played is one below the previous note
+        else if (input_note.num == (prev_note - 1))
+        {
+            scale_down++;
+        }
+        // nothing special happens (i.e. both scales end)
+        else
+        {
+            scale_down = 0;
+            scale_up = 0;
+        }
+
+        prev_note = input_note.num;
+
+        if (scale_up > 3)
+        {
+            StartCoroutine(notification(scale_notification, "Great Scale Up!! +10", on_scale, 10));
+            scale_up = 0;
+        }
+        else if (scale_down > 3)
+        {
+            StartCoroutine(notification(scale_notification, "Great Scale Down!! +10", on_scale, 10));
+            scale_down = 0;
+        }
+    }
+
+    IEnumerator notification(Text notif, string message, bool is_on, int score_increase)
+    {
+        if (!is_on)
+        {
+            notif.text = message;
+            notif.enabled = true;
+            is_on = true;
+            yield return new WaitForSeconds(2);
+            is_on = false;
+            notif.enabled = false;
+        }
+        user_score += score_increase;
     }
 
     void beat_check()
@@ -75,8 +130,8 @@ public class score : MonoBehaviour {
             off_streak = 0;
             if (on_streak > 3)
             {
-                score_notification.text = "On the beat streak! Length " + on_streak.ToString();
-                score_notification.enabled = true;
+                rhythm_notification.text = "On the beat streak! Length " + on_streak.ToString();
+                rhythm_notification.enabled = true;
             }
         }
         else if (bt.checkOffBeat())
@@ -85,15 +140,15 @@ public class score : MonoBehaviour {
             on_streak = 0;
             if (off_streak > 3)
             {
-                score_notification.text = "Off the beat streak!  Length " + off_streak.ToString();
-                score_notification.enabled = true;
+                rhythm_notification.text = "Off the beat streak!  Length " + off_streak.ToString();
+                rhythm_notification.enabled = true;
             }
         }
         else
         {
             on_streak = 0;
             off_streak = 0;
-            score_notification.enabled = false;
+            rhythm_notification.enabled = false;
         }
     }
 
