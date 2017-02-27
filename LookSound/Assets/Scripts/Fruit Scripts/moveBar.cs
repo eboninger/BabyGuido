e55b1b9ca3747c19164fd5c331bd7679fba24886
eschreibman <eliza.schreibman@gmail.com>
2017-02-27 11:10:03 -0500

    Started rhythm bar

---
+++ LookSound/Assets/Scripts/Fruit Scripts/moveBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBar : MonoBehaviour {
+
+	private Vector3 startPos;
+	public float moveRate = 1.2f;
+	public float rightScreenEdge = 8.1f;
+
+	void Start () {
+		startPos = gameObject.transform.position;
+	}
+	
+	void Update () {
+		transform.Translate(Vector3.down * Time.deltaTime * moveRate);
+		if(transform.position.x > rightScreenEdge){
+			transform.position = startPos;
+		}
+	}
+}
---
+++ LookSound/Assets/Scripts/Fruit Scripts/rhythmFruit.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class rhythmFruit : MonoBehaviour {
+
+	public GameObject vertBar;
+	public GameObject horzBar;
+	public GameObject apple;
+	public GameObject banana;
+	public GameObject cherry;
+	public GameObject dragonfruit;
+	public GameObject eggplant;
+	public GameObject fig;
+	public GameObject grape;
+	private float yAxis;
+
+	private Dictionary<char, GameObject> fruit;
+
+	void Start () {
+		yAxis = horzBar.transform.position.y;
+		fruit = new Dictionary<char, GameObject>();
+		fruit.Add('a', dragonfruit);
+		fruit.Add('s', eggplant);
+		fruit.Add('d', fig);
+		fruit.Add('f', grape);
+		fruit.Add('g', apple);
+		fruit.Add('h', banana);
+		fruit.Add('j', cherry);
+		fruit.Add('k', dragonfruit);
+	}
+	
+	void Update () {
+		if (Input.anyKeyDown){
+			foreach (char c in Input.inputString){
+				createFruit(c);
+			}
+		}
+		
+	}
+
+	void createFruit(char c){
+		GameObject newObj;
+		try {
+			newObj = Instantiate(fruit[c]);
+			//move it to the correct position on the staff
+			newObj.transform.position = calculatePosition();
+			fadeOn(newObj);
+		} catch (Exception e) {
+			print("Error: no fruit associated with " + c.ToString());
+		}  
+	}
+
+	Vector3 calculatePosition(){
+		return new Vector3(vertBar.transform.position.x, yAxis, 0f);
+
+	}
+
+	//make the fruit fade over time
+	void fadeOn(GameObject newObj){
+		fadeFruit ff = newObj.GetComponent<fadeFruit>();
+		if(ff != null){
+			ff.turnOnFade();
+		} else{
+			print("Error: no fade fruit script attached to " + newObj.name);
+		}
+	}
+}
